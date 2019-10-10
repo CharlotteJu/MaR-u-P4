0e8b2e5b0816f3b8ae53589fc5dd85e86bed7b12
Charlotte Judon <charlotte.judon@gmail.com>
2019-10-10 10:26:32 +0200

    Permier commit avec classe Reunion

---
+++ MaReu/app/src/main/java/com/example/mareu/Model/Reunion.java
@@ -0,0 +1,71 @@
+package com.example.mareu.Model;
+
+import java.util.Calendar;
+import java.util.List;
+
+public class Reunion
+{
+
+    /**
+     * A list for the participants's emails
+     */
+    private List<String> mEmails;
+
+    /**
+     * A list for the Rooms
+     */
+    private String mRooms;
+
+    /**
+     * To describe the meeting's subject
+     */
+    private String mSubject;
+
+    /**
+     * To put the time
+     */
+    private Calendar mTime;
+
+    public Reunion(List<String> mEmails, String mRooms, String mSubject, Calendar mTime) {
+        this.mEmails = mEmails;
+        this.mRooms = mRooms;
+        this.mSubject = mSubject;
+        this.mTime = mTime;
+    }
+
+    //////////////////// GETTERS ////////////////////
+    public List<String> getmEmails() {
+        return mEmails;
+    }
+
+    public String getmRooms() {
+        return mRooms;
+    }
+
+    public String getmSubject() {
+        return mSubject;
+    }
+
+    public Calendar getmTime() {
+        return mTime;
+    }
+
+
+    //////////////////// SETTERS ////////////////////
+
+    public void setmEmails(List<String> mEmails) {
+        this.mEmails = mEmails;
+    }
+
+    public void setmRooms(String mRooms) {
+        this.mRooms = mRooms;
+    }
+
+    public void setmSubject(String mSubject) {
+        this.mSubject = mSubject;
+    }
+
+    public void setmTime(Calendar mTime) {
+        this.mTime = mTime;
+    }
+}
