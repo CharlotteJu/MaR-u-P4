86d813f738d5d52027a0fa3da6d28709ab7e81ab
Charlotte Judon <charlotte.judon@gmail.com>
2019-10-10 19:13:48 +0200

    Implémentation de la RecyclerView

--- MaReu/app/src/main/java/com/example/mareu/Controler/Fragment/ListReunionsFragment.java
+++ MaReu/app/src/main/java/com/example/mareu/Controler/Fragment/ListReunionsFragment.java
@@ -1,32 +1,68 @@
 package com.example.mareu.Controler.Fragment;
 
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.mareu.Controler.Services.ReunionApiService;
+import com.example.mareu.Controler.Ui.ReunionListRecyclerViewAdapter;
+import com.example.mareu.Model.Reunion;
 import com.example.mareu.R;
+
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ListReunions extends Fragment {
+public class ListReunionsFragment extends Fragment
+{
+    private RecyclerView mRecyclerView;
+    private List<Reunion> mReunions;
+    private ReunionApiService mApiService;
 
-
-    public ListReunions() {
+    public ListReunionsFragment() {
         // Required empty public constructor
     }
 
 
+    public static ListReunionsFragment newInstance()
+    {
+        ListReunionsFragment fragment = new ListReunionsFragment();
+        return fragment;
+    }
+
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list_reunions, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.fragment_list_reunions, container, false);
+        Context context = v.getContext();
+        mRecyclerView = (RecyclerView) v;
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(context));
+        mRecyclerView.addItemDecoration(new DividerItemDecoration(getContext(), DividerItemDecoration.VERTICAL));
+        initList();
+        return v;
+    }
+
+    /**
+     * Init the List of neighbours
+     */
+    private void initList()
+    {
+        mReunions = mApiService.getReunions();
+        mRecyclerView.setAdapter(new ReunionListRecyclerViewAdapter(mReunions));
     }
 
 }
+
--- MaReu/app/src/main/java/com/example/mareu/Controler/Ui/ReunionListRecyclerViewAdapter.java
+++ MaReu/app/src/main/java/com/example/mareu/Controler/Ui/ReunionListRecyclerViewAdapter.java
@@ -1,4 +1,90 @@
 package com.example.mareu.Controler.Ui;
 
-public class ReunionListRecyclerViewAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.mareu.Model.Reunion;
+import com.example.mareu.R;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class ReunionListRecyclerViewAdapter extends RecyclerView.Adapter<ReunionListRecyclerViewAdapter.ViewHolder>
+{
+
+    private List<Reunion> mReunions;
+
+    public ReunionListRecyclerViewAdapter(List<Reunion> mReunions) {
+        this.mReunions = mReunions;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).
+                inflate(R.layout.fragment_one_reunion, parent, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position)
+    {
+        Reunion reunion = mReunions.get(position);
+        holder.updateInfos(reunion);
+    }
+
+
+
+    @Override
+    public int getItemCount() {
+        return mReunions.size();
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder
+    {
+        @BindView(R.id.item_list_name)
+        public TextView mName;
+        @BindView(R.id.item_list_mails)
+        public TextView mMails;
+        @BindView(R.id.item_list_delete_button)
+        public Button mButtonDelete;
+        @BindView(R.id.item_list_avatar)
+        public ImageView mAvatar;
+
+
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+
+        /**
+         * Update the elements of the reunion
+         * @param reunion
+         */
+        public void updateInfos (Reunion reunion)
+        {
+            mName.setText(reunion.getmRooms() + " - " + reunion.getmTime() + " - " + reunion.getmSubject());
+
+            // Cast the list in String
+            String mails = "";
+            for (int i = 0; i < reunion.getmEmails().size(); i ++)
+            {
+                mails += reunion.getmEmails().get(i) + ", ";
+            }
+
+            mMails.setText(mails);
+        }
+    }
 }
