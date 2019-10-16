41ec68eb159d5df150d1aa21b9e341dd9cca9024
Charlotte Judon <charlotte.judon@gmail.com>
2019-10-16 18:10:32 +0200

    Implémentation de 2 des 3 spinners + Model Room + Début de coadage de la date

--- MaReu/app/src/main/java/com/example/mareu/Services/RoomsGenerator.java
+++ MaReu/app/src/main/java/com/example/mareu/Services/RoomsGenerator.java
@@ -1,23 +1,27 @@
 package com.example.mareu.Services;
 
 
+import com.example.mareu.Model.Room;
+import com.example.mareu.R;
+
 public abstract class RoomsGenerator {
 
-    private static String[] LIST_ROOMS = {
-            "Salle A",
-            "Salle B",
-            "Salle C",
-            "Salle D",
-            "Salle E",
-            "Salle F",
-            "Salle G",
-            "Salle H",
-            "Salle I",
-            "Salle J",
-            };
+    private static Room[] LIST_ROOMS = {
+            new Room("Salle 1", R.drawable.room_1),
+            new Room("Salle 2", R.drawable.room_2),
+            new Room("Salle 3", R.drawable.room_3),
+            new Room("Salle 4", R.drawable.room_4),
+            new Room("Salle 5", R.drawable.room_5),
+            new Room("Salle 6", R.drawable.room_6),
+            new Room("Salle 7", R.drawable.room_7),
+            new Room("Salle 8", R.drawable.room_8),
+            new Room("Salle 9", R.drawable.room_9),
+            new Room("Salle 10", R.drawable.room_10),
+    };
 
-    public static String[] generateRooms()
+    public static Room[] generateRooms()
     {
         return LIST_ROOMS;
     }
+
 }
